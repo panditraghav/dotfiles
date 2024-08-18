542b548983f9fd8222a6ca873a85895c6df1f3aa
Raghav Pandit <panditraghav18@gmail.com>
2024-08-18 14:51:12 +0530

    Neovim colorscheme and colorizer update

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,10 @@
 return {
-  { "rebelot/kanagawa.nvim", name = "kanagawa", priority = 1000 },
+  -- { "rebelot/kanagawa.nvim", name = "kanagawa", priority = 1000 },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "catppuccin",
     },
   },
 }
