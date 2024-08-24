8eaaf59016820542c15ea3b27a1e48f13b2c1240
Raghav Pandit <panditraghav18@gmail.com>
2024-08-24 11:07:16 +0530

    Changed nvim colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,12 @@
 return {
-  -- { "rebelot/kanagawa.nvim", name = "kanagawa", priority = 1000 },
+  { "rebelot/kanagawa.nvim", name = "kanagawa", priority = 1000 },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- { "ellisonleao/gruvbox.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      -- colorscheme = "catppuccin",
+      colorscheme = "kanagawa",
     },
   },
 }
