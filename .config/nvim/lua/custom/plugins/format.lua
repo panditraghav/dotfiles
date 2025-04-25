0ddd2334145b0675807b35aa19b46ab7c2ec68c9
Raghav Pandit <panditraghav18@gmail.com>
2025-04-25 17:53:03 +0530

    Neovim prettierd format

--- .config/nvim/lua/custom/plugins/format.lua
+++ .config/nvim/lua/custom/plugins/format.lua
@@ -33,11 +33,15 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        go = { 'gofumpt' },
+        -- c = { 'clang-format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
