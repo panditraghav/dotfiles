466cbcd3e737dfd69d925455b3812ef21bae9ce8
Raghav Pandit <panditraghav@gmail.com>
2025-05-21 08:38:49 +0530

    Changed colors dwm

--- .dwm/config.h
+++ .dwm/config.h
@@ -1,6 +1,5 @@
 /* See LICENSE file for copyright and license details. */
 #include <X11/XF86keysym.h>
-
 /* appearance */
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -13,10 +12,14 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_brown[]	    = "#2f1d0f";
+static const char col_border[]      = "#70411d";
+static const char col_light1[]	    = "#e5ddca";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_light1, col_brown,  col_gray2  },
+	[SchemeSel]  = { col_gray4, col_brown,  col_border },
 };
 
 /* tagging */
